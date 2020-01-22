edd47c4d47ec2209019188b0377bae8acccaf716
Troy Wiegand <troy@troywiegand.com>
2020-01-22 13:43:55 +0000

    Refactor params.pp in favor of Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,8 @@
 # @summary 
 #  Installs the base Apache package
 # 
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package{ "${install_name}":
-    ensure => $install_ensure,
+class apache::install {
+  package{ "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
