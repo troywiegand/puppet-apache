74a39d83fd077ef766d735e2a41bc15a77ca490d
Troy Wiegand <troy@troywiegand.com>
2020-01-22 15:38:49 +0000

    Add a service class

---
+++ manifests/service.pp
@@ -0,0 +1,10 @@
+# @summary
+#   Allows for the Apache service to restart when triggered
+class apache::service {
+  service { "${apache::service_name}":
+    alias      => 'apache_service',
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
+    hasrestart => true,
+  }
+}
