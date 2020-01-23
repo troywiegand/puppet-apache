7c0a47c2c789825a6297e8f2682301a57953be5f
Troy Wiegand <troy@troywiegand.com>
2020-01-23 15:54:50 +0000

    Add Facts to our Module

---
+++ lib/facter/apache_processes.rb
@@ -0,0 +1,11 @@
+Facter.add(:apache_processes) do
+    setcode do
+        osfamily = Facter.value(:osfamily)
+        case osfamily
+        when 'Debian'
+            Facter::Core::Execution.execute('pgrep apache2 | wc -l ')
+        when 'RedHat'
+            Facter::Core::Execution.execute('pgrep httpd | wc -l ')
+        end
+    end
+end
